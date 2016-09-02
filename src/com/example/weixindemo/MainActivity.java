373c22a964b259561e2763fff123b715050afcb5
francioshu <shufu.shu@gmail.com>
2016-09-02 16:26:07 +0800

    update

--- src/com/example/weixindemo/MainActivity.java
+++ src/com/example/weixindemo/MainActivity.java
@@ -183,7 +183,7 @@ public class MainActivity extends Activity implements OnClickListener {
 		String text = "欢迎光临，请吩咐";
 
 		// mTts.startSpeaking(text, mTtsListener);
-		sendGril(text);
+		send(text);
 
 		isshow = true;
 
@@ -610,7 +610,7 @@ public class MainActivity extends Activity implements OnClickListener {
 		public void onError(SpeechError error) {
 			Log.d(TAG, error.getPlainDescription(true));
 			mTts.startSpeaking("您好像没说话哦！", mTtsListener);
-			sendGril("您好像没说话哦！");
+			send("您好像没说话哦！");
 			mCircleWaveView.setVisibility(View.GONE);
 			stat = STATUS_None;
 		}
